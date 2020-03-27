d2cb3fe5817feb746ed1566c11b4e954a16761b3
Markus Larsson <mark.larsson.93@gmail.com>
2020-03-27 19:30:23 +0100

    Scrapping the individual orgs, going with WHO only

--- main.py
+++ main.py
@@ -1,58 +1,18 @@
-import requests, re
+import requests, PyPDF2, io
 from bs4 import BeautifulSoup
 
-# cdc testing
-def get_cdc():
-	parsed_cdc = BeautifulSoup(requests.get("https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html").content, "html.parser")
-	parsed_cdc_cases = parsed_cdc.find("div", {"class": "2019coronavirus-summary"}).findChildren("li")
-	numResults = []
-	blacklist = ['Jurisdictions', 'reporting', 'states', 'district']
 
-	for i in range(len(parsed_cdc_cases)):
-		canAppend = True
-		s = parsed_cdc_cases[i].get_text()
+parsed = BeautifulSoup(requests.get("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports").content, "html.parser")
+parsed_cases = parsed.find("div", {"id": "PageContent_C006_Col01"}).findChildren("p")
 
-		for i2 in range(len(blacklist)):
-			if s.find(blacklist[i]) != -1:
-				canAppend = False
-				break
+pdf_urls = []
 
-		if canAppend:
-			numResults.append(int(''.join(re.findall("\d+", s))))
+for i in range(len(parsed_cases)):
+	pdf_urls.append("https://www.who.int" + parsed_cases[i].findChildren("a")[0].get('href'))
+	break
 
-	numResults.sort(reverse=True)
+response = requests.get(pdf_urls[0])
 
-	return {'cases': numResults[0], 'deaths': numResults[1]}
+pdf = PyPDF2.PdfFileReader(io.BytesIO(response.content))
 
-print(get_cdc())
-
-# folkhälsomyndigheten testing
-#https://experience.arcgis.com/experience/09f821667ce64bf7be6f9f87457ed9aa
-parsed_fhm = BeautifulSoup(requests.get("https://fohm.maps.arcgis.com/apps/opsdashboard/index.html").content, "html.parser")
-print(parsed_fhm.find("iframe-widget_1"))
-parsed_fhm_cases = parsed_fhm.find("full-container", {"class": "layout-reference"}).findChildren("text")
-numResults = []
-blacklist = ['Intensivvårdade']
-
-for i in range(len(parsed_fhm_cases)):
-	canAppend = True
-	s = parsed_fhm_cases[i].get_text()
-
-	for i2 in range(len(blacklist)):
-		if s.find(blacklist[i]) != -1:
-			canAppend = False
-			break
-
-	if canAppend:
-		numResults.append(int(''.join(re.findall("\d+", s))))
-
-numResults.sort()
-print(numResults)
-
-
-# typical structure should follow:
-# 1. request with a GET
-# 2. parse with BeautifulSoup 4 using the html.parser
-# 3. find the appropriate div with relevant class (if possible)
-# 4. loop through the list and add text to a list
-# 5. parse the list for cases and deaths and add to two separate self vars
+print(pdf.getPage(0).extractText())
